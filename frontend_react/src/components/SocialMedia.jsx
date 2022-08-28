6cbc64b000dfa28673a2b5633037bea81fa91c0f
Nhut Ly <nhutly@Brown-Bubble-Tea.local>
2022-08-28 15:29:09 -0500

    add resume icon

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import { BsTwitter, BsInstagram } from 'react-icons/bs';
 import { FaGithub } from 'react-icons/fa';
+import { IoMdDocument} from 'react-icons/io';
+
+
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+      <IoMdDocument />
     </div>
     <div>
       <a href="https://github.com/nhutly" target="_blank">
         <FaGithub />      
       </a>   
     </div>
-    <div>
+    {/* <div>
       <BsInstagram />
-    </div>
+    </div> */}
   </div>
 );
 
